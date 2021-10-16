2652e99c0b7a32662c0f74d6fbfb6c1ae21f4129
iman <imanaida135@gmail.com>
2021-10-16 23:38:57 +0300

    first

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -10,5 +10,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
